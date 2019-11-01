c1e05aa381a468b94cdf6798eb94b878dcaeeb47
Xinci Chen <chenxinci@dhcp-wifi-8021x-155-41-61-136.bu.edu>
2019-11-01 16:32:01 -0400

    commit

---
+++ check.R
@@ -0,0 +1,5 @@
+#check
+courses <- read.csv("~/Desktop/MA615/class26/courses.csv",header = TRUE,stringsAsFactors = FALSE)
+
+enrollment <- read.csv("~/Desktop/MA615/class26/enrollment.csv",header = TRUE,stringsAsFactors = FALSE) 
+
